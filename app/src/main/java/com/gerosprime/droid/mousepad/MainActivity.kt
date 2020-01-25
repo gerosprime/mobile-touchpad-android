0df988dc2cf24c2ee67f0b6ab54198dbc2c9cb95
Glenn Ross Pascual <glenn.development@gmail.com>
2020-01-25 19:46:58 +0800

    Project init with feature modules.

---
+++ app/src/main/java/com/gerosprime/droid/mousepad/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.gerosprime.droid.mousepad
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import java.net.DatagramSocket
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        
+    }
+}
